0a972be278c84778402bdfa442b84c75b1943986
Ninjdai1 <ninjdai@duck.com>
2023-10-04 14:02:37 +0200

    Added more classes

---
+++ src/inventory/inventory.py
@@ -0,0 +1,25 @@
+from .item import Item
+
+class Inventory:
+    """Base inventory class.
+
+    Meant to be used as an inventory to store data on entities
+
+    Attributes:
+        contents: A list of Item classes storing items contained in the inventory
+    """
+    def __init__(self, item_list: list[Item] = None) -> None:
+        self.contents = []
+        if not item_list is None and type(item_list) == list:
+            self.contents += item_list
+
+    def add_item(self, item : Item, count = 1) -> None:
+        """Adds an item to the inventory, adds it to the item count if item is stackable"""
+        assert type(item) == Item, "item must be an instance of the Item class !"
+        if item in self.contents and item.stackable:
+            item.count += 1
+        else:
+            self.contents.append(item)
+
+
+
---
+++ src/inventory/item.py
@@ -0,0 +1,6 @@
+
+class Item:
+    count = 1
+    stackable = True
+    def __init__(self) -> None:
+        pass
