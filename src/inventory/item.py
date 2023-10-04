b803d70d1497e4179dd71078684112cb9f3df681
Ninjdai <65647523+Ninjdai1@users.noreply.github.com>
2023-10-04 15:03:21 +0200

    Update item.py
    Add documentation

--- src/inventory/item.py
+++ src/inventory/item.py
@@ -1,5 +1,13 @@
 
 class Item:
+    """Base item class.
+
+    Meant to be used as a way to store data concerning items
+
+    Attributes:
+        count: Integer, quantity of item stored, mostly used for inventory management
+        stackable: Boolean, defines whether or not items of the same typed can be grouped in inventory
+    """
     count = 1
     stackable = True
     def __init__(self) -> None:
