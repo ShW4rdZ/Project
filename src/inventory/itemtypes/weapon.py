da7863e8a393f86fcc10e96ca269fc6b167ed603
Ninjdai1 <ninjdai@duck.com>
2023-10-06 16:10:48 +0200

    Added attack speed attribute to weapon class

--- src/inventory/itemtypes/weapon.py
+++ src/inventory/itemtypes/weapon.py
@@ -2,8 +2,8 @@ from . import Equipment
 
 class Weapon(Equipment):
     """Weapon class that derives from Equipment, helps with creating weapons"""
-    def __init__(self, **kwargs) -> None:
-        
+    def __init__(self, attack_speed: float = 1, **kwargs) -> None:
+        self.attack_speed = attack_speed # Attack speed: attack_speed attacks per second
         super().__init__(**kwargs)
 
     def __repr__(self) -> str:
