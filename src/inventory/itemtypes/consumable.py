61f7d5f74013f729de24a967f83c0b2c64304b6a
Ninjdai1 <ninjdai@duck.com>
2023-10-08 18:17:29 +0200

    Add documentation for consumables and add uses_left attribute to consumables

--- src/inventory/itemtypes/consumable.py
+++ src/inventory/itemtypes/consumable.py
@@ -1,13 +1,21 @@
 from .. import Item
 
 class Consumable(Item):
-    """Consumable class that derives from Item, comes out of the box with effects and misc attributes for consumable creation"""
-    def __init__(self, effects: list = [], duration: float = 1, **kwargs) -> None:
+    """Consumable class
+    
+    Derives from Item, comes out of the box with effects and misc attributes for consumable creation
+
+    Attributes:
+        effects: list of effects triggered on consumable use
+        duration: duration of the effects (in seconds). 0 means instant effects. Defaults to 60
+        uses_left: how many uses are left on the consumable. Defaults to 1"""
+    def __init__(self, effects: list = [], duration: float = 60, uses: int = 1 **kwargs) -> None:
         self.effects = effects
-        self.duration = duration
+        self.duration = duration #In seconds
+        self.uses_left = uses
 
         super().__init__(**kwargs)
 
     def __repr__(self) -> str:
-        return f"<Consumable({self.name} - {self.effects} for {self.duration}m)>"
+        return f"<Consumable({self.name} - {self.effects} for {self.duration}s)>"
 
