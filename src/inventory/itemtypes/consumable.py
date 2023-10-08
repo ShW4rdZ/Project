af72d6566fad97d88446438d082d9d396cae9f0f
Ninjdai1 <ninjdai@duck.com>
2023-10-08 18:21:03 +0200

    Fix typo

--- src/inventory/itemtypes/consumable.py
+++ src/inventory/itemtypes/consumable.py
@@ -9,7 +9,7 @@ class Consumable(Item):
         effects: list of effects triggered on consumable use
         duration: duration of the effects (in seconds). 0 means instant effects. Defaults to 60
         uses_left: how many uses are left on the consumable. Defaults to 1"""
-    def __init__(self, effects: list = [], duration: float = 60, uses: int = 1 **kwargs) -> None:
+    def __init__(self, effects: list = [], duration: float = 60, uses: int = 1, **kwargs) -> None:
         self.effects = effects
         self.duration = duration #In seconds
         self.uses_left = uses
